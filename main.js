f068aabc100f0f6353caf404c06ac56f650c8076
John Lim (Win Desktop) <limjohn@uw.edu>
2017-09-13 14:35:24 -0700

    better resize check

--- main.js
+++ main.js
@@ -44,6 +44,15 @@ function initCustomHover() {
 	jQuery('.stuff-carousel .slide').touchHover({});
 }
 
+function initIsMobile() {
+    var isMobile = false; //initiate as false
+// device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+
+    window.isMobile = isMobile;
+}
+
 function initNavAddClass() {
 	var win = jQuery(window);
 	var footer = jQuery('#footer');
@@ -313,6 +322,7 @@ function initCanvas() {
 		var winWidth, winHeight;
 		var mp; //max particles
 		var particles = [];
+		var cached_window_width = 0;
 
 		resizeHandler();
 
@@ -328,6 +338,7 @@ function initCanvas() {
 			}
 			ctx.fill();
 			update();
+            requestAnimationFrame(draw);
 		}
 
 		function update() {
@@ -344,14 +355,20 @@ function initCanvas() {
 		}
 
 		function resizeHandler() {
+
+
 			//canvas dimensions
 			winWidth = window.innerWidth;
 			winHeight = window.innerHeight;
 
+            if (window.isMobile && cached_window_width == winWidth) {
+            	return;
+			}
+
 			canvas.width = winWidth;
 			canvas.height = winHeight;
 
-			mp = 0.35 * winWidth;
+			mp = 0.25 * winWidth;
 
 			particles = [];
 
@@ -363,18 +380,12 @@ function initCanvas() {
 					d: Math.random() * mp //density
 				})
 			}
+            cached_window_width = winWidth;
 		}
 
 		win.on('resize', resizeHandler);
 
-		function step() {
-			setTimeout(function() {
-				draw();
-				requestAnimationFrame(step);
-			}, 1200 / fps);
-		}
-
-		step();
+        requestAnimationFrame(draw);
 	});
 }
 
@@ -1761,339 +1772,6 @@ ResponsiveHelper = (function($){
 
 }));
 
- /*!
-
- * JavaScript Custom Forms : Checkbox Module
-
- *
-
- * Copyright 2014-2015 PSD2HTML - http://psd2html.com/jcf
-
- * Released under the MIT license (LICENSE.txt)
-
- *
-
- * Version: 1.1.3
-
- */
-
-;(function($) {
-
-	'use strict';
-
-
-
-	jcf.addModule({
-
-		name: 'Checkbox',
-
-		selector: 'input[type="checkbox"]',
-
-		options: {
-
-			wrapNative: true,
-
-			checkedClass: 'jcf-checked',
-
-			uncheckedClass: 'jcf-unchecked',
-
-			labelActiveClass: 'jcf-label-active',
-
-			fakeStructure: '<span class="jcf-checkbox"><span></span></span>'
-
-		},
-
-		matchElement: function(element) {
-
-			return element.is(':checkbox');
-
-		},
-
-		init: function() {
-
-			this.initStructure();
-
-			this.attachEvents();
-
-			this.refresh();
-
-		},
-
-		initStructure: function() {
-
-			// prepare structure
-
-			this.doc = $(document);
-
-			this.realElement = $(this.options.element);
-
-			this.fakeElement = $(this.options.fakeStructure).insertAfter(this.realElement);
-
-			this.labelElement = this.getLabelFor();
-
-
-
-			if (this.options.wrapNative) {
-
-				// wrap native checkbox inside fake block
-
-				this.realElement.appendTo(this.fakeElement).css({
-
-					position: 'absolute',
-
-					height: '100%',
-
-					width: '100%',
-
-					opacity: 0,
-
-					margin: 0
-
-				});
-
-			} else {
-
-				// just hide native checkbox
-
-				this.realElement.addClass(this.options.hiddenClass);
-
-			}
-
-		},
-
-		attachEvents: function() {
-
-			// add event handlers
-
-			this.realElement.on({
-
-				focus: this.onFocus,
-
-				click: this.onRealClick
-
-			});
-
-			this.fakeElement.on('click', this.onFakeClick);
-
-			this.fakeElement.on('jcf-pointerdown', this.onPress);
-
-		},
-
-		onRealClick: function(e) {
-
-			// just redraw fake element (setTimeout handles click that might be prevented)
-
-			var self = this;
-
-			this.savedEventObject = e;
-
-			setTimeout(function() {
-
-				self.refresh();
-
-			}, 0);
-
-		},
-
-		onFakeClick: function(e) {
-
-			// skip event if clicked on real element inside wrapper
-
-			if (this.options.wrapNative && this.realElement.is(e.target)) {
-
-				return;
-
-			}
-
-
-
-			// toggle checked class
-
-			if (!this.realElement.is(':disabled')) {
-
-				delete this.savedEventObject;
-
-				this.stateChecked = this.realElement.prop('checked');
-
-				this.realElement.prop('checked', !this.stateChecked);
-
-				this.fireNativeEvent(this.realElement, 'click');
-
-				if (this.savedEventObject && this.savedEventObject.isDefaultPrevented()) {
-
-					this.realElement.prop('checked', this.stateChecked);
-
-				} else {
-
-					this.fireNativeEvent(this.realElement, 'change');
-
-				}
-
-				delete this.savedEventObject;
-
-			}
-
-		},
-
-		onFocus: function() {
-
-			if (!this.pressedFlag || !this.focusedFlag) {
-
-				this.focusedFlag = true;
-
-				this.fakeElement.addClass(this.options.focusClass);
-
-				this.realElement.on('blur', this.onBlur);
-
-			}
-
-		},
-
-		onBlur: function() {
-
-			if (!this.pressedFlag) {
-
-				this.focusedFlag = false;
-
-				this.fakeElement.removeClass(this.options.focusClass);
-
-				this.realElement.off('blur', this.onBlur);
-
-			}
-
-		},
-
-		onPress: function(e) {
-
-			if (!this.focusedFlag && e.pointerType === 'mouse') {
-
-				this.realElement.focus();
-
-			}
-
-			this.pressedFlag = true;
-
-			this.fakeElement.addClass(this.options.pressedClass);
-
-			this.doc.on('jcf-pointerup', this.onRelease);
-
-		},
-
-		onRelease: function(e) {
-
-			if (this.focusedFlag && e.pointerType === 'mouse') {
-
-				this.realElement.focus();
-
-			}
-
-			this.pressedFlag = false;
-
-			this.fakeElement.removeClass(this.options.pressedClass);
-
-			this.doc.off('jcf-pointerup', this.onRelease);
-
-		},
-
-		getLabelFor: function() {
-
-			var parentLabel = this.realElement.closest('label'),
-
-				elementId = this.realElement.prop('id');
-
-
-
-			if (!parentLabel.length && elementId) {
-
-				parentLabel = $('label[for="' + elementId + '"]');
-
-			}
-
-			return parentLabel.length ? parentLabel : null;
-
-		},
-
-		refresh: function() {
-
-			// redraw custom checkbox
-
-			var isChecked = this.realElement.is(':checked'),
-
-				isDisabled = this.realElement.is(':disabled');
-
-
-
-			this.fakeElement.toggleClass(this.options.checkedClass, isChecked)
-
-							.toggleClass(this.options.uncheckedClass, !isChecked)
-
-							.toggleClass(this.options.disabledClass, isDisabled);
-
-
-
-			if (this.labelElement) {
-
-				this.labelElement.toggleClass(this.options.labelActiveClass, isChecked);
-
-			}
-
-		},
-
-		destroy: function() {
-
-			// restore structure
-
-			if (this.options.wrapNative) {
-
-				this.realElement.insertBefore(this.fakeElement).css({
-
-					position: '',
-
-					width: '',
-
-					height: '',
-
-					opacity: '',
-
-					margin: ''
-
-				});
-
-			} else {
-
-				this.realElement.removeClass(this.options.hiddenClass);
-
-			}
-
-
-
-			// removing element will also remove its event handlers
-
-			this.fakeElement.off('jcf-pointerdown', this.onPress);
-
-			this.fakeElement.remove();
-
-
-
-			// remove other event handlers
-
-			this.doc.off('jcf-pointerup', this.onRelease);
-
-			this.realElement.off({
-
-				focus: this.onFocus,
-
-				click: this.onRealClick
-
-			});
-
-		}
-
-	});
-
-
-
-}(jQuery));
 
 $.fn.parallax = function ( resistance, mouse )
 {
